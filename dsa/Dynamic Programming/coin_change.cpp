e12d487581ea960d8428fa6b4fc726354772ce65
Visharad Jaydas Borsutkar <visharad@Visharads-MacBook-Air.local>
2024-06-16 10:52:48 +0530

    closes #1; added coin change problem

--- dsa/Dynamic Programming/coin_change.cpp
+++ dsa/Dynamic Programming/coin_change.cpp
@@ -1,6 +1,6 @@
 /*
- * unbounded knapsack problem
  * coin change problem
+ * unbounded knapsack problem
  * problem : give a coin array calculate number of ways to pick coins to get given sum, given unlimited supply of each coin
  * */
 
@@ -9,24 +9,39 @@
 #include <vector>
 using namespace std;
 
-
 void coin_change_dp(vector<int> coins, int sum){
-    const int n = coins.size();
-    //  4    4   4   4
-    // | 1 | 2 | 3 | 4 |
-    // | 3 | 4 | 5 | 7 |
-    // | 2 | 4 | 5 | 6 |
-    
+    //  sapce and time - O(n x sum)
+    //  dp table ------
+    // n\s ----->
+    // |   0   1   2   3   4
+    //   | 1 | 0 | 0 | 0 | 0 |
+    //   | 1 | 1 | 1 | 1 | 1 |
+    //   | 1 | 1 | 2 | 2 | 3 |
+    //   | 1 | 1 | 2 | 3 | 4 |
 
+    const int n = coins.size();
+    vector<vector<int>> dp(n+1, vector<int>(sum+1));
+
+    for(int i=0; i<=n; i++) dp[i][0] = 1;
+    for(int j=1; j<=sum; j++) dp[0][j] = 0;
+
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=sum; j++){
+            dp[i][j] = dp[i-1][j];
+            if(coins[i-1] <= j) dp[i][j] += dp[i][j-coins[i-1]];
+        }
+    }
+    cout << "dp answers :" << dp[n][sum] << "\n";
 }
 
+// naive recursive solution
 int recur_sol(vector<int> coins,int n, int sum){
     // two actions per coin - take the coin, do not take the coin
     if(sum == 0) return 1;
     if(sum < 0) return 0;
     if(n == 0) return 0;
     // consider the last coin or do not consider the last coin 
-    //1. take the coin hence sum reduced 2. do not take the coin hence number of coins reduced
+    //1. take the coin hence sum reduced 2. do not take the coin hence number of coins option reduced
     return recur_sol(coins, n, sum-coins[n-1]) + recur_sol(coins, n-1, sum);
 }
 
--- theory/operating systems/introduction.md
+++ theory/operating systems/introduction.md
@@ -10,7 +10,7 @@ operating systems ( linux, windows)
             |
 hardware (CPU, HDD, memory)
 
-____________________________________________
+__________________________________________________________________________________________
 
 - Reason for operating system to exists 
     - Resource mangement : We have limited resource for a particular machine. it manages which application or process to run in backgroud. handles
@@ -18,5 +18,19 @@ ____________________________________________
     - Abstraction : operating system extracts the hardware ( talks to them), we don't need to write machine/binary code. It provides system calls to
                     the library, using which application development becomes easier
     - Protection : protects application from each other, protects from malware. kind of system manager
+
+__________________________________________________________________________________________
+
+#### Types of Operating systems
+- Single tasking 
+- Mutli-programming and Mutli-tasking 
+- Multi-threading
+- Multi-processing
+
+#### single tasking :
+    - very basic/simple operating systems
+    - only one process can exists in RAM and can run at a time 
+    - cons : CPU has to sit ideal during IO
+    - ex. MS DOS
 
 
