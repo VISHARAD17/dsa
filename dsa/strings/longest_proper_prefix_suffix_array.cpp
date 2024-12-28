c5b2f585aeb929f9606543c426c605bd301ff56b
Visharad Jaydas Borsutkar <visharadborsutkar777@gmail.com>
2024-12-28 14:03:59 +0530

    leetcode problem
    3370 [easy]

---
+++ dsa/strings/longest_proper_prefix_suffix_array.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h> 
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+const ll MOD = 1e9+7;
+#define all(x) (x).begin(), (x).end()
+
+class Solution{
+/*
+ * perfect prefix of "abcd" -> "", "a", "ab", "abc"
+ * suffix of "abcd" -> "", "d", "cd", "bcd", "abcd"
+ * */
+public:
+    void LongestProperPrefixSuffix(vector<int>&nums){
+        const int n = nums.size();
+    }
+
+};
+
+int main()
+{
+    Solution st;
+    return 0;
+}
