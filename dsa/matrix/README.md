41a25a88cef2d47d429cee7bbd5ff16ed2b61f8f
Visharad Jaydas Borsutkar <visharadborsutkar777@gmail.com>
2024-12-31 17:14:43 +0530

    finished matrix from gfg course

--- dsa/matrix/README.md
+++ dsa/matrix/README.md
@@ -1,5 +1,11 @@
 ### Matrix
 
+- matrix representation in c++, size n x m with all elements initialized as 0
+    
+     `vector<vector<int>> matrix(n, vector<int>(m, 0))`
+-   
+- dynamic size vectors, no defined row size `vector<int> arr[m]`, total m dynamic rows
+
 
 Problems:
 - [transpose of a matrix]()
--- dsa/matrix/print_in_snake_pattern.cpp
+++ dsa/matrix/print_in_snake_pattern.cpp
@@ -14,24 +14,19 @@ const ll MOD = 1e9+7;
 
 class Solution{
 public:
-    /* given n x n matrix, print its transpose, -> 1st row become 1st col, etc.*/
-    void transposeOfMatrix(vector<vector<int>> &matrix){
+    // print matrix in snake pattern i.e row in reverse of the previous row
+    void printInSnakePattern(vector<vector<int>> &matrix){
         const int row = matrix.size();
         const int col = matrix[0].size();
-
-        // swap elements along the diagonal
         for(int i=0; i<row; i++){
-            for(int j=i+1; j<col; j++){
-                int tmp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = tmp;
+            if(i%2 == 0){
+                for(int j=0; j<col; j++) cout << matrix[i][j] << " ";
             }
-        }
-        for(auto &x: matrix){
-            for(auto &y: x) cout << y << " ";
+            else{
+                for(int j=col-1; j>=0; j--) cout << matrix[i][j] << " ";
+            }
             cout << "\n";
         }
-
     }
 
 };
@@ -43,6 +38,6 @@ int main()
                                 {9, 10, 11, 12}, 
                                 {13, 14, 15, 16}};
     Solution st;
-    st.transposeOfMatrix(matrix);
+    st.printInSnakePattern(matrix);
     return 0;
 }
